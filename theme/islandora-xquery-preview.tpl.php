47ac9c6dbbe236d9c269e7a9d0feff94451df64f
vagrant <vagrant@daniellamb-vagrant-i7latest.53090.i7latest.dgicloud.com>
2014-04-13 20:25:32 +0000

    Generating diffs using xdiff.  Displaying diffs in highlighted green and red using GeSHi.  Pages.

--- theme/islandora-xquery-preview.tpl.php
+++ theme/islandora-xquery-preview.tpl.php
@@ -1,10 +1,4 @@
-<?php for ($i = 0; $i < $results->rowCount(); $i++): ?>
-  <div class="islandora-xquery-diff">
-    <?php $result = $results->fetchObject(); ?>
-    <h2><?php print $result->pid; ?></h2>
-    <pre><?php print $result->preview; ?></pre>
-  </div>
-  <?php if ($i < $results->rowCount() - 1): ?>
-    <hr />
-  <?php endif ?>
-<?php endfor ?>
+<div class="islandora-xquery-diff">
+  <h2><?php print $result->pid; ?></h2>
+  <?php print $result->GeSHi->parse_code(); ?>
+</div>
