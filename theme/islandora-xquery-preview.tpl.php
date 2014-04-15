3934516cfe8f2308146956fa9df1d61c172c7ae6
vagrant <vagrant@daniellamb-vagrant-i7latest.53090.i7latest.dgicloud.com>
2014-04-15 12:10:06 +0000

    Code standards.  Cancelling.  Massive Cleanup.

--- theme/islandora-xquery-preview.tpl.php
+++ theme/islandora-xquery-preview.tpl.php
@@ -1,4 +1,42 @@
-<div class="islandora-xquery-diff">
-  <h2><?php print $result->pid; ?></h2>
-  <?php print $result->GeSHi->parse_code(); ?>
+<?php
+/**
+ * @file
+ * Template to render out all diffs for a given xquery.
+ */
+?>
+
+<?php libraries_load("geshi"); ?>
+
+<div class="islandora-xquery-preview">
+    <?php print l(t("APPLY"),
+                  "islandora/xquery/preview/$batch_id/apply",
+                  array(
+                    'attributes' => array(
+                      'class' => array(
+                        'islandora-xquery-apply-link',
+                      ),
+                    ),
+                  )
+                 );
+          print " ";
+          print l(t("CANCEL"),
+                  "islandora/xquery/preview/$batch_id/cancel",
+                  array(
+                    'attributes' => array(
+                      'class' => array(
+                        'islandora-xquery-cancel-link',
+                      ),
+                    ),
+                  )
+                 );
+    ?>
+    <?php foreach($results as $result): ?>
+        <div class="islandora-xquery-diff">
+          <h2><?php print $result->pid; ?></h2>
+          <?php $geshi = new GeSHi($result->preview, 'diff');
+                print $geshi->parse_code();
+          ?>
+        </div>
+    <?php endforeach ?>
+    <?php print theme('pager', array('tags' => array())); ?>
 </div>
